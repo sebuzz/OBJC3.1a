7c5f0a4b870b0e9ed9c074fb73a166e5fb83a6e6
Sebastian Künne <sebuzz@sebastians-mbp.fritz.box>
2016-03-18 14:49:24 +0100

    Initial Commit

---
+++ OBJC3.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OBJC3.1
+//
+//  Created by Sebastian Künne on 18.03.16.
+//  Copyright © 2016 Sebastian Künne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
